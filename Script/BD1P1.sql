37cd5d0c5f2fa6d9257d26c41638bf7f7e5cca2d
bryan967132 <bryan.tejaxun@gmail.com>
2024-02-25 22:03:51 -0600

    script

---
+++ Script/BD1P1.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE BD1P1;
+USE BD1P1;
+
+CREATE TABLE Test(
+	id INT PRIMARY KEY,
+    texto VARCHAR(255)
+);
+
+INSERT INTO Test(id, texto) VALUES(1, "Hola Mundo");
+INSERT INTO Test(id, texto) VALUES(2, "Es Una Prueba");
+
+SELECT * FROM Test;
