ab17b43a2b1a519b191d81148d8ae8bba8501f7b
bryan967132 <bryan.tejaxun@gmail.com>
2024-03-09 02:13:11 -0600

    Controlador

--- API/Go/Controller/Controller.go
+++ API/Go/Controller/Controller.go
@@ -194,38 +194,21 @@ func (c *Controller) Postdatamodel(ctx *fiber.Ctx) error {
 				continue
 			}
 			if i == 0 { // Pais
-				_, err := c.DB.Exec(fmt.Sprintf(`INSERT INTO pais (id, nombre) VALUE (%s, "%s")`, line[0], line[1]))
-				if err != nil {
-					fmt.Println("Error al insertar pais", err)
-				}
+				c.DB.Exec(fmt.Sprintf(`INSERT INTO pais (id, nombre) VALUE (%s, "%s")`, line[0], line[1]))
 			} else if i == 1 { // Categoria
-				_, err := c.DB.Exec(fmt.Sprintf(`INSERT INTO categoria (id, nombre) VALUE (%s, "%s")`, line[0], line[1]))
-				if err != nil {
-					fmt.Println("Error al insertar categoria", err)
-				}
+				c.DB.Exec(fmt.Sprintf(`INSERT INTO categoria (id, nombre) VALUE (%s, "%s")`, line[0], line[1]))
 			} else if i == 2 { // Cliente
-				_, err := c.DB.Exec(fmt.Sprintf(`INSERT INTO cliente (id, nombre, apellido, direccion, telefono, tarjeta, edad, salario, genero, pais_id) VALUE (%s, "%s", "%s", "%s", "%s", "%s", %s, %s, "%s", %s)`, line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9]))
-				if err != nil {
-					fmt.Println("Error al insertar cliente", err)
-				}
+				c.DB.Exec(fmt.Sprintf(`INSERT INTO cliente (id, nombre, apellido, direccion, telefono, tarjeta, edad, salario, genero, pais_id) VALUE (%s, "%s", "%s", "%s", "%s", "%s", %s, %s, "%s", %s)`, line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9]))
 			} else if i == 3 { // Vendedor
 				fullName := strings.Split(line[1], " ")
-				_, err := c.DB.Exec(fmt.Sprintf(`INSERT INTO vendedor (id, nombre, apellido, pais_id) VALUE (%s, "%s", "%s", %s)`, line[0], fullName[0], fullName[1], line[2]))
-				if err != nil {
-					fmt.Println("Error al insertar vendedor", err)
-				}
+				c.DB.Exec(fmt.Sprintf(`INSERT INTO vendedor (id, nombre, apellido, pais_id) VALUE (%s, "%s", "%s", %s)`, line[0], fullName[0], fullName[1], line[2]))
 			} else if i == 4 { // Producto
-				_, err := c.DB.Exec(fmt.Sprintf(`INSERT INTO producto (id, nombre, precio, categoria_id) VALUE (%s, "%s", %s, %s)`, line[0], line[1], line[2], line[3]))
-				if err != nil {
-					fmt.Println("Error al insertar producto", err)
-				}
+				c.DB.Exec(fmt.Sprintf(`INSERT INTO producto (id, nombre, precio, categoria_id) VALUE (%s, "%s", %s, %s)`, line[0], line[1], line[2], line[3]))
 			} else { // Orden
 				date := strings.Split(line[2], "/")
 				line[2] = fmt.Sprintf("%s-%s-%s", date[2], date[1], date[0])
-				_, err := c.DB.Exec(fmt.Sprintf(`INSERT INTO orden (id, linea, fecha, cantidad, cliente_id, vendedor_id, producto_id) VALUE (%s, '%s', %s, %s, %s, %s, %s)`, line[0], line[1], line[2], line[6], line[3], line[4], line[5]))
-				if err != nil {
-					fmt.Println("Error al insertar orden", err)
-				}
+				c.DB.Exec(fmt.Sprintf(`INSERT INTO orden (id, fecha, cliente_id) SELECT %s, '%s', %s FROM orden WHERE id != %s`, line[0], line[2], line[3], line[0]))
+				c.DB.Exec(fmt.Sprintf(`INSERT INTO datoorden (linea, cantidad, orden_id, vendedor_id, producto_id) VALUE (%s, %s, %s, %s, %s)`, line[1], line[6], line[0], line[4], line[5]))
 			}
 		}
 	}
