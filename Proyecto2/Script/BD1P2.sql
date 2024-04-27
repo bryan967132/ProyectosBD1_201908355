36e8b6bbdd656311ad24d26c698c0a473989884b
bryan967132 <bryan.tejaxun@gmail.com>
2024-04-27 02:58:29 -0600

    Script

--- Proyecto2/Script/BD1P2.sql
+++ Proyecto2/Script/BD1P2.sql
@@ -8,19 +8,19 @@ CREATE TABLE IF NOT EXISTS BD1P2.TipoBien (
 );
 
 CREATE TABLE IF NOT EXISTS BD1P2.TipoCliente (
-    id          INTEGER PRIMARY KEY NOT NULL,
-    nombre      VARCHAR(20) NOT NULL,
-    descripcion VARCHAR(40)
+    id          INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nombre      VARCHAR(40) NOT NULL,
+    descripcion VARCHAR(200)
 );
 
 CREATE TABLE IF NOT EXISTS BD1P2.TipoCuenta (
-    id          INTEGER PRIMARY KEY NOT NULL,
-    nombre      VARCHAR(20) NOT NULL,
-    descripcion VARCHAR(100) NOT NULL
+    id          INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nombre      VARCHAR(40) NOT NULL,
+    descripcion VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS BD1P2.TipoTransaccion (
-    id          INTEGER PRIMARY KEY NOT NULL,
+    id          INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nombre      VARCHAR(20) NOT NULL,
     descripcion VARCHAR(40) NOT NULL
 );
@@ -37,35 +37,38 @@ CREATE TABLE IF NOT EXISTS BD1P2.Cliente (
     id             INTEGER PRIMARY KEY NOT NULL,
     nombre         VARCHAR(40) NOT NULL,
     apellidos      VARCHAR(40) NOT NULL,
-    telefono       VARCHAR(12) NOT NULL,
-    correo         VARCHAR(40) NOT NULL,
     usuario        VARCHAR(40) NOT NULL,
     contrasena     VARCHAR(200) NOT NULL,
+    fechacreacion  DATE NOT NULL,
     tipocliente_id INTEGER NOT NULL,
     FOREIGN KEY (tipocliente_id) REFERENCES BD1P2.TipoCliente (id)
+);
+
+CREATE TABLE IF NOT EXISTS BD1P2.Telefono (
+	id         INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    numero     VARCHAR(12),
+    cliente_id INTEGER,
+    FOREIGN KEY (cliente_id) REFERENCES BD1P2.Cliente (id)
+);
+
+CREATE TABLE IF NOT EXISTS BD1P2.Correo (
+	id         INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    direccion  VARCHAR(100),
+    cliente_id INTEGER,
+    FOREIGN KEY (cliente_id) REFERENCES BD1P2.Cliente (id)
 );
 
 CREATE TABLE IF NOT EXISTS BD1P2.Cuenta (
     id             INTEGER PRIMARY KEY NOT NULL,
     monto_apertura FLOAT(2) NOT NULL,
     saldo          FLOAT(2) NOT NULL,
-    descripcion    VARCHAR(50) NOT NULL,
+    descripcion    VARCHAR(255) NOT NULL,
     fecha_apertura DATE NOT NULL,
-    otros_detalles VARCHAR(100),
+    otros_detalles VARCHAR(255),
     tipocuenta_id  INTEGER NOT NULL,
     cliente_id     INTEGER NOT NULL,
     FOREIGN KEY (tipocuenta_id) REFERENCES BD1P2.TipoCuenta (id),
     FOREIGN KEY (cliente_id) REFERENCES BD1P2.Cliente (id)
-);
-
-CREATE TABLE IF NOT EXISTS BD1P2.Transaccion (
-    id                 INTEGER PRIMARY KEY NOT NULL,
-    fecha              DATE NOT NULL,
-    otros_detalles     VARCHAR(40),
-    cuenta_id          INTEGER NOT NULL,
-    tipotransaccion_id INTEGER NOT NULL,
-    FOREIGN KEY (cuenta_id) REFERENCES Cuenta (id),
-    FOREIGN KEY (tipotransaccion_id) REFERENCES BD1P2.TipoTransaccion (id)
 );
 
 CREATE TABLE IF NOT EXISTS BD1P2.Compra (
@@ -73,15 +76,10 @@ CREATE TABLE IF NOT EXISTS BD1P2.Compra (
     fecha          DATE NOT NULL,
     importe        FLOAT,
     otros_detalles VARCHAR(100),
-    transaccion_id INTEGER NOT NULL,
-    FOREIGN KEY (transaccion_id) REFERENCES BD1P2.Transaccion (id)
-);
-
-CREATE TABLE IF NOT EXISTS BD1P2.CompraDetalle (
-    compra_id INTEGER NOT NULL,
-    bien_id   INTEGER NOT NULL,
-    FOREIGN KEY (compra_id) REFERENCES BD1P2.Compra (id),
-    FOREIGN KEY (bien_id) REFERENCES BD1P2.Bien (id)
+    bien_id INTEGER NOT NULL,
+    cliente_id INTEGER NOT NULL,
+    FOREIGN KEY (bien_id) REFERENCES BD1P2.Bien (id),
+    FOREIGN KEY (cliente_id) REFERENCES BD1P2.Cliente (id)
 );
 
 CREATE TABLE IF NOT EXISTS BD1P2.Debito (
@@ -89,8 +87,8 @@ CREATE TABLE IF NOT EXISTS BD1P2.Debito (
     fecha          DATE NOT NULL,
     monto          FLOAT(2) NOT NULL,
     otros_detalles VARCHAR(40),
-    transaccion_id INTEGER NOT NULL,
-    FOREIGN KEY (transaccion_id) REFERENCES BD1P2.Transaccion (id)
+    cliente_id     INTEGER NOT NULL,
+    FOREIGN KEY (cliente_id) REFERENCES BD1P2.Cliente (id)
 );
 
 CREATE TABLE IF NOT EXISTS BD1P2.Deposito (
@@ -98,8 +96,24 @@ CREATE TABLE IF NOT EXISTS BD1P2.Deposito (
     fecha          DATE NOT NULL,
     monto          FLOAT(2) NOT NULL,
     otros_detalles VARCHAR(40),
-    transaccion_id INTEGER NOT NULL,
-    FOREIGN KEY (transaccion_id) REFERENCES BD1P2.Transaccion (id)
+    cliente_id     INTEGER NOT NULL,
+    FOREIGN KEY (cliente_id) REFERENCES BD1P2.Cliente (id)
+);
+
+CREATE TABLE IF NOT EXISTS BD1P2.Transaccion (
+    id                 INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    fecha              DATE NOT NULL,
+    otros_detalles     VARCHAR(40),
+    cuenta_id          INTEGER NOT NULL,
+    tipotransaccion_id INTEGER NOT NULL,
+    deposito_id        INTEGER,
+    debito_id          INTEGER,
+    compra_id          INTEGER,
+    FOREIGN KEY (cuenta_id) REFERENCES Cuenta (id),
+    FOREIGN KEY (tipotransaccion_id) REFERENCES BD1P2.TipoTransaccion (id),
+    FOREIGN KEY (deposito_id) REFERENCES BD1P2.Deposito (id),
+    FOREIGN KEY (debito_id) REFERENCES BD1P2.Debito (id),
+    FOREIGN KEY (compra_id) REFERENCES BD1P2.Compra (id)
 );
 
 CREATE TABLE IF NOT EXISTS BD1P2.Historial (
@@ -107,3 +121,45 @@ CREATE TABLE IF NOT EXISTS BD1P2.Historial (
     descripcion VARCHAR(100),
     tipo        VARCHAR(50)
 );
+
+INSERT INTO BD1P2.TipoCliente (nombre, descripcion) VALUES
+('Individual Nacional', 'Este tipo de cliente es una persona individual de nacionalidad guatemalteca.'),
+('Individual Extranjero', 'Este tipo de cliente es una persona individual de nacionalidad extranjera.'),
+('Empresa PyMe', 'Este tipo de cliente es una empresa de tipo pequeña o mediana.'),
+('Empresa S.C', 'Este tipo de cliente corresponde a las empresa grandes que tienen una sociedad colectiva.');
+
+INSERT INTO BD1P2.TipoCuenta (nombre, descripcion) VALUES
+('Cuenta de Cheques', 'Este tipo de cuenta ofrece la facilidad de emitir cheques para realizar transacciones monetarias.'),
+('Cuenta de Ahorros', 'Esta cuenta genera un interés anual del 2%, lo que la hace ideal para guardar fondos a largo plazo.'),
+('Cuenta de Ahorro Plus', 'Con una tasa de interés anual del 10%, esta cuenta de ahorros ofrece mayores rendimientos.'),
+('Pequeña Cuenta', 'Una cuenta de ahorros con un interés semestral del 0.5%, ideal para pequeños ahorros y movimientos.'),
+('Cuenta de Nómina', 'Diseñada para recibir depósitos de sueldo y realizar pagos, con acceso a servicios bancarios básicos.'),
+('Cuenta de Inversión', 'Orientada a inversionistas, ofrece opciones de inversión y rendimientos más altos que una cuenta de ahorros estándar.');
+
+INSERT INTO BD1P2.TipoTransaccion (id, nombre, descripcion) VALUES
+(1, 'Compra', 'Transacción de compra'),
+(2, 'Deposito', 'Transacción de deposito'),
+(3, 'Debito', 'Transacción de debito');
+
+INSERT INTO BD1P2.TipoBien (id, nombre) VALUES
+(1, 'Servicio'),
+(2, 'Producto');
+
+INSERT INTO BD1P2.Bien (id, costo, descripcion, tipobien_id) VALUES
+(1, 10.00, 'Servicio de tarjeta de debito', 1),
+(2, 10.00, 'Servicio de chequera', 1),
+(3, 400.00, 'Servicio de asesoramiento financiero', 1),
+(4, 5.00, 'Servicio de banca personal', 1),
+(5, 30.00, 'Seguro de vida', 1),
+(6, 100.00, 'Seguro de vida plus', 1),
+(7, 300.00, 'Seguro de automóvil', 1),
+(8, 500.00, 'Seguro de automóvil plus', 1),
+(9, 0.05, 'Servicio de deposito', 1),
+(10, 0.10, 'Servicio de Debito', 1),
+(11, 0, 'Pago de energía Eléctrica (EEGSA)', 2),
+(12, 0, 'Pago de agua potable (Empagua)', 2),
+(13, 0, 'Pago de Matricula USAC', 2),
+(14, 0, 'Pago de curso vacaciones USAC', 2),
+(15, 0, 'Pago de servicio de internet', 2),
+(16, 0, 'Servicio de suscripción plataformas streaming', 2),
+(17, 0, 'Servicios Cloud', 2);
---
+++ Proyecto2/Script/Procs.sql
@@ -0,0 +1,435 @@
+DROP PROCEDURE IF EXISTS BD1P2.registrarTipoCliente;     -- 1
+DROP PROCEDURE IF EXISTS BD1P2.registrarTipoCuenta;      -- 2
+DROP PROCEDURE IF EXISTS BD1P2.registrarTipoTransaccion; -- 3
+DROP PROCEDURE IF EXISTS BD1P2.crearProductoServicio;    -- 4
+DROP FUNCTION IF EXISTS BD1P2.procesarTelefonos;
+DROP PROCEDURE IF EXISTS BD1P2.registrarTelefonos;
+DROP FUNCTION IF EXISTS BD1P2.procesarCorreos;
+DROP PROCEDURE IF EXISTS BD1P2.registrarCorreos;
+DROP PROCEDURE IF EXISTS BD1P2.registrarCliente;         -- 5
+DROP PROCEDURE IF EXISTS BD1P2.registrarCuenta;          -- 6
+DROP PROCEDURE IF EXISTS BD1P2.realizarCompra;           -- 7
+DROP PROCEDURE IF EXISTS BD1P2.realizarDeposito;         -- 8
+DROP PROCEDURE IF EXISTS BD1P2.realizarDebito;           -- 9
+DROP PROCEDURE IF EXISTS BD1P2.asignarTransaccion;       -- 10
+
+DELIMITER //
+
+-- registrarTipoCliente
+CREATE PROCEDURE BD1P2.registrarTipoCliente(
+	IN idTC          INTEGER,
+	IN nombreTC      VARCHAR(40),
+	IN descripcionTC VARCHAR(200)
+) BEGIN
+	IF descripcionTC REGEXP '^[[:alpha:]]+$' THEN
+		INSERT INTO BD1P2.TipoCliente (nombre, descripcion) VALUES
+		(nombreTC, descripcionTC);
+	ELSE
+		SELECT '¡LA DESCRIPCIÓN DEL TIPO DE CLIENTE DEBE CONTENER SOLO LETRAS!';
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡LA DESCRIPCIÓN DEL TIPO DE CLIENTE DEBE CONTENER SOLO LETRAS!';
+	END IF;
+END //
+-- registrarTipoCuenta
+CREATE PROCEDURE BD1P2.registrarTipoCuenta(
+	IN idTC          INTEGER,
+	IN nombreTC      VARCHAR(40),
+	IN descripcionTC VARCHAR(200)
+) BEGIN
+	INSERT INTO BD1P2.TipoCuenta (nombre, descripcion) VALUES
+	(nombreTC, descripcionTC);
+END //
+-- registrarTipoTransaccion
+CREATE PROCEDURE BD1P2.registrarTipoTransaccion(
+	IN idTC          INTEGER,
+	IN nombreTC      VARCHAR(40),
+	IN descripcionTC VARCHAR(255)
+) BEGIN
+	INSERT INTO BD1P2.TipoTransaccion (nombre, descripcion) VALUES
+	(nombreTC, descripcionTC);
+END //
+-- crearProductoServicio
+CREATE PROCEDURE BD1P2.crearProductoServicio(
+	IN idPS          INTEGER,
+	IN tipoPS        INTEGER,
+	IN costoPS       FLOAT(2),
+	IN descripcionPS VARCHAR(200)
+) BEGIN
+	IF tipoPS = 1 AND costoPS > 0 OR tipoPS = 2 AND costPS >= 0 THEN
+		INSERT INTO BD1P2.Bien (id, costo, descripcion, tipobien_id) VALUES
+		(idPS, costoPS, descripcionPS, tipoPS);
+	ELSE IF tipoPS = 1 AND costoPS <= 0 THEN
+		SELECT '¡COSTO INCORRECTO PARA UN SERVICIO!';
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡COSTO INCORRECTO PARA UN SERVICIO!';
+	ELSE IF tipoPS = 2 AND costPS < 0 THEN
+		SELECT '¡COSTO INCORRECTO PARA UN PRODUCTO!';
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡COSTO INCORRECTO PARA UN PRODUCTO!';
+	END IF;
+	END IF;
+	END IF;
+END //
+-- procesarTelefonos
+CREATE FUNCTION BD1P2.procesarTelefonos(
+	cadena VARCHAR(255)
+) RETURNS VARCHAR(255) DETERMINISTIC BEGIN
+	DECLARE telefono            VARCHAR(12);
+	DECLARE telefonosProcesados VARCHAR(255);
+	DECLARE telefonoProcesado   VARCHAR(12);
+	SET telefonosProcesados = '';
+	WHILE CHAR_LENGTH(cadena) > 0 DO
+		SET telefono = SUBSTRING_INDEX(cadena, '-', 1);
+		IF CHAR_LENGTH(telefono) > 8 THEN
+			SET telefonoProcesado = SUBSTRING(telefono, 4);
+		ELSE
+			SET telefonoProcesado = telefono;
+		END IF;
+		IF CHAR_LENGTH(telefonosProcesados) > 0 THEN
+			SET telefonosProcesados = CONCAT(telefonosProcesados, '-', telefonoProcesado);
+		ELSE
+			SET telefonosProcesados = telefonoProcesado;
+		END IF;
+		SET cadena = SUBSTRING(cadena, CHAR_LENGTH(telefono) + 2);
+	END WHILE;
+	RETURN telefonosProcesados;
+END //
+-- registrarTelefonos
+CREATE PROCEDURE BD1P2.registrarTelefonos(
+	IN cadena    VARCHAR(255),
+	IN clienteid INTEGER
+) BEGIN
+	DECLARE telefono            VARCHAR(12);
+	DECLARE telefonosProcesados VARCHAR(255);
+	DECLARE telefonoProcesado   VARCHAR(12);
+	SET telefonosProcesados = '';
+	WHILE CHAR_LENGTH(cadena) > 0 DO
+		SET telefono = SUBSTRING_INDEX(cadena, '-', 1);
+		INSERT INTO BD1P2.Telefono (numero, cliente_id) VALUE (telefono, clienteid);
+		SET cadena = SUBSTRING(cadena, CHAR_LENGTH(telefono) + 2);
+	END WHILE;
+END //
+-- procesarCorreos
+CREATE FUNCTION BD1P2.procesarCorreos(
+	cadena VARCHAR(255)
+) RETURNS BOOLEAN DETERMINISTIC BEGIN
+	DECLARE correo            VARCHAR(100);
+	DECLARE correosProcesados VARCHAR(255);
+	DECLARE correoValido      BOOLEAN;
+	SET correosProcesados = '';
+	WHILE CHAR_LENGTH(cadena) > 0 DO
+		SET correo = SUBSTRING_INDEX(cadena, '|', 1);
+		IF correo REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$' THEN
+			SET correoValido = TRUE;
+		ELSE
+			SET correoValido = FALSE;
+		END IF;
+		IF NOT correoValido THEN
+			RETURN FALSE;
+		END IF;
+		SET cadena = SUBSTRING(cadena, CHAR_LENGTH(correo) + 2);
+	END WHILE;
+	RETURN TRUE;
+END //
+-- registrarCorreos
+CREATE PROCEDURE BD1P2.registrarCorreos(
+	IN cadena VARCHAR(255),
+	IN clienteid INTEGER
+) BEGIN
+	DECLARE correo            VARCHAR(100);
+	DECLARE correosProcesados VARCHAR(255);
+	DECLARE correoValido      BOOLEAN;
+	SET correosProcesados = '';
+	WHILE CHAR_LENGTH(cadena) > 0 DO
+		SET correo = SUBSTRING_INDEX(cadena, '|', 1);
+		INSERT INTO BD1P2.Correo (direccion, cliente_id) VALUE (correo, clienteid);
+		SET cadena = SUBSTRING(cadena, CHAR_LENGTH(correo) + 2);
+	END WHILE;
+END //
+-- registrarCliente
+SET @clave = 'sisalebases1';
+CREATE PROCEDURE BD1P2.registrarCliente(
+	IN idC         INTEGER,
+	IN nombreC     VARCHAR(40),
+	IN apellidosC  VARCHAR(40),
+	IN telefonosC  VARCHAR(255),
+	IN correosC    VARCHAR(40),
+	IN usuarioC    VARCHAR(40),
+	IN contrasenaC VARCHAR(200),
+	IN idtipoC     INTEGER
+) BEGIN
+	DECLARE correos_validos      BOOLEAN;
+	DECLARE telefonos_procesados VARCHAR(255);
+	DECLARE usuarioUsado         BOOLEAN DEFAULT FALSE;
+	IF nombreC REGEXP '^[[:alpha:]]+$' THEN
+		IF apellidosC REGEXP '^[[:alpha:]]+$' THEN
+			SELECT TRUE INTO usuarioUsado FROM BD1P2.Cliente WHERE usuario = usuarioC;
+			IF usuarioUsado THEN
+				SET correos_validos = BD1P2.procesarCorreos(correosC);
+				IF corres_validos THEN
+					SET telefonos_procesados = BD1P2.procesarTelefonos(telefonosC);
+					INSERT INTO BD1P2.Cliente (id, nombre, apellidos, usuario, contrasena, fechacreacion, tipocliente_id) VALUES
+					(idC, nombreC, apellidosC, usuarioC, HEX(AES_ENCRYPT(contrasenaC, @clave)), CURDATE(), idtipoC);
+				ELSE
+					SELECT '¡El formato del o los correos del usuario que intenta registrar no son correctos!';
+					SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡El formato del o los correos del usuario que intenta registrar no son correctos!';
+				END IF;
+			ELSE
+				SELECT '¡El nombre de usuario ya se encuentra en uso!';
+				SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡El nombre de usuario ya se encuentra en uso!';
+			END IF;
+		ELSE
+			SELECT '¡El apellido del usuario que intenta registrar contiene caracteres extraños!';
+			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡El apellido del usuario que intenta registrar contiene caracteres extraños!';
+		END IF;
+	ELSE
+		SELECT '¡El nombre del usuario que intenta registrar contiene caracteres extraños!';
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡El nombre del usuario que intenta registrar contiene caracteres extraños!';
+	END IF;
+END //
+-- registrarCuenta
+CREATE PROCEDURE BD1P2.registrarCuenta(
+	IN idC          INTEGER,
+	IN montoApC     FLOAT(2),
+	IN saldoC       FLOAT(2),
+	IN descripcionC VARCHAR(255),
+	IN fechaApC     VARCHAR(30),
+	IN otrosdetC    VARCHAR(255),
+	IN tipocuentaC  INTEGER,
+	IN idclienteC   INTEGER
+) BEGIN
+	DECLARE idtipocuenta INTEGER DEFAULT -1;
+	DECLARE idcliente    INTEGER DEFAULT -1;
+	IF montoApC > 0 THEN
+		IF saldoC >= 0 THEN
+			SELECT id INTO idtipocuenta FROM BD1P2.TipoCuenta WHERE id = tipocuentaC;
+			IF idtipocuenta != -1 THEN
+				SELECT id INTO idcliente FROM BD1P2.Cliente WHERE id = idclienteC;
+				IF idcliente != -1 THEN
+					INSERT INTO BD1P2.Cuenta (id, monto_apertura, saldo, descripcion, fecha_apertura, otros_detalles, tipocuenta_id, cliente_id) VALUES
+					(idC, montoApC, saldoC, descripcionC, NOW(), otrosdetC, tipocuentaC, idclienteC);
+				ELSE
+					SELECT '¡Intenta vincular la cuenta a un cliente inexistente!';
+					SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡Intenta vincular la cuenta a un cliente inexistente!';
+				END IF;
+			ELSE
+				SELECT '¡Intenta asignar un tipo inexistente a la cuenta!';
+				SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡Intenta asignar un tipo inexistente a la cuenta!';
+			END IF;
+		ELSE
+			SELECT '¡El saldo para la nueva cuenta debe ser una cantidad positiva!';
+			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡El saldo para la nueva cuenta debe ser una cantidad positiva!';
+		END IF;
+	ELSE
+		SELECT '¡El monto de apertura para la nueva cuenta debe ser una cantidad positiva!';
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡El monto de apertura para la nueva cuenta debe ser una cantidad positiva!';
+	END IF;
+END //
+-- realizarCompra
+CREATE PROCEDURE BD1P2.realizarCompra(
+	IN idC            INTEGER,
+	IN fechaC         VARCHAR(30),
+	IN importeC       FLOAT(2),
+	IN otrosDetallesC VARCHAR(255),
+	IN idbienC        INTEGER,
+	IN idclienteC     INTEGER
+) BEGIN
+	DECLARE tipo      INTEGER DEFAULT -1;
+	DECLARE idcliente INTEGER DEFAULT -1;
+	SELECT tipobien_id INTO tipo FROM BD1P2.Bien WHERE id = idbienC;
+	SELECT id INTO idcliente FROM BD1P2.Cliente WHERE id = idclienteC;
+	IF tipo = 2 THEN
+		IF importeC > 0 THEN
+			IF idcliente != -1 THEN
+				INSERT INTO BD1P2.Compra (id, fecha, importe, otros_detalles, bien_id, cliente_id) VALUE
+				(idC, STR_TO_DATE(fechaC, '%d/%m/%Y'), importeC, otrosDetallesC, idbienC, idclienteC);
+			ELSE
+				SELECT '¡No existe el cliente que quiere registrar en la compra!';
+				SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡No existe el cliente que quiere registrar en la compra!';
+			END IF;
+		ELSE
+			SELECT '¡El importe de la compra de un producto debe ser una cantidad positiva mayor que cero!';
+			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡El importe de la compra de un producto debe ser una cantidad positiva mayor que cero!';
+		END IF;
+	ELSE IF tipo = 1 THEN
+		IF importeC = 0 THEN
+			IF idcliente != -1 THEN
+				INSERT INTO BD1P2.Compra (id, fecha, importe, otros_detalles, bien_id, cliente_id) VALUE
+				(idC, STR_TO_DATE(fechaC, '%d/%m/%Y'), importeC, otrosDetallesC, idbienC, idclienteC);
+			ELSE
+				SELECT '¡No existe el cliente que quiere registrar en la compra!';
+				SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡No existe el cliente que quiere registrar en la compra!';
+			END IF;
+		ELSE
+			SELECT '¡El importe de la compra de un servicio debe ser igual a cero!';
+			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡El importe de la compra de un servicio debe ser igual a cero!';
+		END IF;
+	ELSE
+		SELECT '¡Solo existen 1. Servicio 2. Producto para realizar una compra!';
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡Solo existen 1. Servicio 2. Producto para realizar una compra!';
+	END IF;
+	END IF;
+END //
+-- realizarDeposito
+CREATE PROCEDURE BD1P2.realizarDeposito(
+	IN idD            INTEGER,
+	IN fechaD         VARCHAR(30),
+	IN montoD         FLOAT(2),
+	IN otrosDetallesD VARCHAR(255),
+	IN idclienteD     INTEGER
+) BEGIN
+	DECLARE existeUsuario BOOLEAN DEFAULT FALSE;
+	SELECT TRUE INTO existeUsuario FROM BD1P2.Cliente WHERE id = idclienteD;
+	IF existeUsuario THEN
+		IF montoD > 0 THEN
+			INSERT INTO BD1P2.Deposito (id, fecha, monto, otros_detalles, cliente_id) value
+			(idD, STR_TO_DATE(fechaD, '%d/%m/%Y'), montoD, otrosDetallesD, idclienteD);
+		ELSE
+			SELECT '¡El monto del depósito debe ser mayor que cero!';
+			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡El monto del depósito debe ser mayor que cero!';
+		END IF;
+	ELSE
+		SELECT '¡No existe el usuario al que quiere vincular el depósito!';
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡No existe el usuario al que quiere vincular el depósito!';
+	END IF;
+END //
+-- realizarDebito
+CREATE PROCEDURE BD1P2.realizarDebito(
+	IN idD            INTEGER,
+	IN fechaD         VARCHAR(30),
+	IN montoD         FLOAT(2),
+	IN otrosDetallesD VARCHAR(255),
+	IN idclienteD     INTEGER
+) BEGIN
+	DECLARE existeUsuario BOOLEAN DEFAULT FALSE;
+	SELECT TRUE INTO existeUsuario FROM BD1P2.Cliente WHERE id = idclienteD;
+	IF existeUsuario THEN
+		IF montoD > 0 THEN
+			INSERT INTO BD1P2.Debito (id, fecha, monto, otros_detalles, cliente_id) value
+			(idD, STR_TO_DATE(fechaD, '%d/%m/%Y'), montoD, otrosDetallesD, idclienteD);
+		ELSE
+			SELECT '¡El monto del débito debe ser mayor que cero!';
+			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡El monto del débito debe ser mayor que cero!';
+		END IF;
+	ELSE
+		SELECT '¡No existe el usuario al que quiere vincular el débito!';
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡No existe el usuario al que quiere vincular el débito!';
+	END IF;
+END //
+-- asignarTransaccion
+CREATE PROCEDURE BD1P2.asignarTransaccion(
+	IN idT              INTEGER,
+	IN fechaT           VARCHAR(30),
+	IN otrosDetallesD   VARCHAR(255),
+	IN tipotransaccionD INTEGER,
+	IN idaccionD        INTEGER,
+	IN idcuentaD        INTEGER
+) BEGIN
+	DECLARE existeCuenta      INTEGER DEFAULT FALSE;
+	DECLARE tipotransaccion   INTEGER DEFAULT -1;
+	DECLARE existetransaccion BOOLEAN DEFAULT FALSE;
+	DECLARE esCuentacorrecta  BOOLEAN DEFAULT FALSE;
+	DECLARE saldoCuenta       FLOAT(2) DEFAULT 0;
+	DECLARE montoTransaccion  FLOAT(2) DEFAULT 0;
+	SELECT id FROM BD1P2.TipoTransaccion WHERE id = tipotransaccionD;
+	IF existeCuenta THEN
+		IF tipotransaccion != -1 THEN
+			IF tipotransaccion = 1 THEN -- COMPRA
+				SELECT TRUE INTO existetransaccion FROM BD1P2.Compra WHERE id = idaccionD;
+			ELSE IF tipotransaccion = 2 THEN -- DEPOSITO
+				SELECT TRUE INTO existetransaccion FROM BD1P2.Deposito WHERE id = idaccionD;
+			ELSE IF tipotransaccion = 3 THEN -- DEPOSITO
+				SELECT TRUE INTO existetransaccion FROM BD1P2.Debito WHERE id = idaccionD;
+			END IF;
+			END IF;
+			END IF;
+			IF existetransaccion THEN
+				IF tipotransaccion = 1 THEN -- COMPRA
+					SELECT TRUE INTO esCuentacorrecta FROM BD1P2.Cuenta c
+					INNER JOIN BD1P2.Compra co ON c.cliente_id = co.cliente_id
+					WHERE c.id = idcuentaD;
+				ELSE IF tipotransaccion = 2 THEN -- DEPOSITO
+					SELECT TRUE INTO esCuentacorrecta FROM BD1P2.Cuenta c
+					INNER JOIN BD1P2.Deposito d ON c.cliente_id = d.cliente_id
+					WHERE c.id = idcuentaD;
+				ELSE IF tipotransaccion = 3 THEN -- DEPOSITO
+					SELECT TRUE INTO esCuentacorrecta FROM BD1P2.Cuenta c
+					INNER JOIN BD1P2.Debito d ON c.cliente_id = d.cliente_id
+					WHERE c.id = idcuentaD;
+				END IF;
+				END IF;
+				END IF;
+				IF esCuentacorrecta THEN
+					IF tipotransaccion = 1 OR tipotransaccion = 3 THEN
+						SELECT saldo INTO saldoCuenta FROM BD1P2.Cuenta WHERE id = idcuentaD;
+					END IF;
+					IF tipotransaccion = 1 THEN -- COMPRA
+						SELECT importe INTO montoTransaccion FROM BD1P2.Compra WHERE id = idaccionD;
+					ELSE IF tipotransaccion = 2 THEN -- DEPOSITO
+						SELECT monto INTO montoTransaccion FROM BD1P2.Deposito WHERE id = idaccionD;
+					ELSE IF tipotransaccion = 3 THEN -- DEPOSITO
+						SELECT monto INTO montoTransaccion FROM BD1P2.Debito WHERE id = idaccionD;
+					END IF;
+					END IF;
+					END IF;
+					IF tipotransaccion = 1 OR tipotransaccion = 3 THEN -- COMPRA O DEBITO
+						IF saldoCuenta >= montoTransaccion THEN
+							UPDATE BD1P2.Cuenta SET saldo = saldo - montoTransaccion WHERE id = idcuentaD;
+							IF tipotransaccion = 1 THEN
+								INSERT INTO BD1P2.Transaccion (fecha, otros_detalles , cuenta_id, tipotransaccion_id, compra_id) VALUE
+								(STR_TO_DATE(fechaT, '%d/%m/%Y'), otrosDetallesD, idcuentaD, tipotransaccionD, idaccionD);
+							ELSE IF tipotransaccion = 3 THEN
+								INSERT INTO BD1P2.Transaccion (fecha, otros_detalles , cuenta_id, tipotransaccion_id, debito_id) VALUE
+								(STR_TO_DATE(fechaT, '%d/%m/%Y'), otrosDetallesD, idcuentaD, tipotransaccionD, idaccionD);
+							END IF;
+							END IF;
+						ELSE
+							IF tipotransaccion = 1 THEN -- COMPRA
+								SELECT '¡El saldo en la cuenta es insuficiente para realizar la compra!';
+								SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡El saldo en la cuenta es insuficiente para realizar la compra!';
+							ELSE IF tipotransaccion = 3 THEN -- DEPOSITO
+								SELECT '¡El saldo en la cuenta es insuficiente para realizar el débito!';
+								SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡El saldo en la cuenta es insuficiente para realizar el débito!';
+							END IF;
+							END IF;
+						END IF;
+					ELSE -- DEPOSITO
+						UPDATE BD1P2.Cuenta SET saldo = saldo + montoTransaccion WHERE id = idcuentaD;
+						INSERT INTO BD1P2.Transaccion (fecha, otros_detalles , cuenta_id, tipotransaccion_id, deposito_id) VALUE
+						(STR_TO_DATE(fechaT, '%d/%m/%Y'), otrosDetallesD, idcuentaD, tipotransaccionD, idaccionD);
+					END IF;
+				ELSE
+					IF tipotransaccion = 1 THEN -- COMPRA
+						SELECT '¡La cuenta y la compra no corresponden al mismo cliente!';
+						SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡La cuenta y la compra no corresponden al mismo cliente!';
+					ELSE IF tipotransaccion = 2 THEN -- DEPOSITO
+						SELECT '¡La cuenta y el depósito no corresponden al mismo cliente!';
+						SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡La cuenta y el depósito no corresponden al mismo cliente!';
+					ELSE IF tipotransaccion = 3 THEN -- DEPOSITO
+						SELECT '¡La cuenta y el débito no corresponden al mismo cliente!';
+						SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡La cuenta y el débito no corresponden al mismo cliente!';
+					END IF;
+					END IF;
+					END IF;
+				END IF;
+			ELSE
+				IF tipotransaccion = 1 THEN -- COMPRA
+					SELECT '¡No existe la compra que quiere vincular a la transacción!';
+					SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡No existe la compra que quiere vincular a la transacción!';
+				ELSE IF tipotransaccion = 2 THEN -- DEPOSITO
+					SELECT '¡No existe el depósito que quiere vincular a la transacción!';
+					SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡No existe el depósito que quiere vincular a la transacción!';
+				ELSE IF tipotransaccion = 3 THEN -- DEPOSITO
+					SELECT '¡No existe el débito que quiere vincular a la transacción!';
+					SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡No existe el débito que quiere vincular a la transacción!';
+				END IF;
+				END IF;
+				END IF;
+			END IF;
+		ELSE
+			SELECT '¡No existe el tipo de transaccion que quiere registrar!';
+			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡No existe el tipo de transaccion que quiere registrar!';
+		END IF;
+	ELSE
+		SELECT '¡No existe la cuenta con la que quiere realizar una transacción!';
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '¡No existe la cuenta con la que quiere realizar una transacción!';
+	END IF;
+END //
+DELIMITER ;
